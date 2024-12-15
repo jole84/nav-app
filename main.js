4e866c7492e95097faf09abf0e510470f401a877
jole84 <johanlennartsson84@gmail.com>
2024-12-15 19:07:50 +0100

    moved to separate function

--- main.js
+++ main.js
@@ -1638,20 +1638,7 @@ function updateUserPosition() {
   }
 }
 
-// document.getElementById("tripPointButton").addEventListener("click", function () {
-//   line.appendCoordinate(view.getCenter());
-//   trackLog.push([toLonLat(view.getCenter()), altitude, new Date()]);
-//   console.log(trackLog)
-// });
-
-const trackPointLayer = new VectorLayer({
-  source: new VectorSource(),
-  style: gpxStyleText,
-  declutter: true,
-});
-map.addLayer(trackPointLayer);
-
-line.addEventListener("change", function () {
+document.getElementById("tripPointButton").addEventListener("click", function () {
   trackPointLayer.getSource().clear();
   for (let i = 1; i < trackLog.length; i++) {
     const distanceToNext = getDistance(trackLog[i - 1][0], trackLog[i][0]);
@@ -1664,3 +1651,10 @@ line.addEventListener("change", function () {
     trackPointLayer.getSource().addFeature(marker);
   }
 });
+
+const trackPointLayer = new VectorLayer({
+  source: new VectorSource(),
+  style: gpxStyleText,
+  declutter: true,
+});
+map.addLayer(trackPointLayer);
